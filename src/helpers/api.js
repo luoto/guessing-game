bfafd562994713d58c46daccc294595cd17debaf
Tony Luo <luo.tony37@gmail.com>
2018-11-24 21:21:48 -0800

    Use environmental variable to control api usage

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,6 +3,7 @@ const proxy = 'https://cors-anywhere.herokuapp.com/';
 const endpoint = 'http://app.linkedin-reach.io/words';
 
 const NUMBER_OF_WORDS = 162413;
+const DEFAULT_WORD = 'banana';
 const DIFFICULTIES = {
   easy: 2,
   medium: 5,
@@ -11,19 +12,23 @@ const DIFFICULTIES = {
 
 const api = {
   getWord: options => {
-    const randomIndex = Math.floor(Math.random() * NUMBER_OF_WORDS);
-    const queryString = `?start=${randomIndex}&count=1&difficulty=${
-      DIFFICULTIES[options.difficulty]
-    }`;
+    if (process.env.REACT_APP_USE_LIVE_API.toLowerCase() === 'true') {
+      const randomIndex = Math.floor(Math.random() * NUMBER_OF_WORDS);
+      const queryString = `?start=${randomIndex}&count=1&difficulty=${
+        DIFFICULTIES[options.difficulty]
+      }`;
 
-    return axios
-      .get(`${proxy}${endpoint}${queryString}`)
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
+      return axios
+        .get(`${proxy}${endpoint}${queryString}`)
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    } else {
+      return DEFAULT_WORD;
+    }
   }
 };
 
