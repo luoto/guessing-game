2279354825b232c654a2e02c718031eb36893365
Tony Luo <luo.tony37@gmail.com>
2018-11-27 20:53:36 -0800

    Updates

---
+++ precache-manifest.7b07e8559357f07c0810b4e96710939f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "baffc29f3af61ede29d9",
+    "url": "/guessing-game/static/js/runtime~main.baffc29f.js"
+  },
+  {
+    "revision": "f048c67511b9fa8c1f87",
+    "url": "/guessing-game/static/js/main.f048c675.chunk.js"
+  },
+  {
+    "revision": "d3d4e871ef374af7ef7d",
+    "url": "/guessing-game/static/js/1.d3d4e871.chunk.js"
+  },
+  {
+    "revision": "f048c67511b9fa8c1f87",
+    "url": "/guessing-game/static/css/main.7b21fcce.chunk.css"
+  },
+  {
+    "revision": "d3d4e871ef374af7ef7d",
+    "url": "/guessing-game/static/css/1.a2b0c06e.chunk.css"
+  },
+  {
+    "revision": "4de561674db324d28f339698833d4bcd",
+    "url": "/guessing-game/index.html"
+  }
+];
